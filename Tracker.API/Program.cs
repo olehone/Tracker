a0b00d1e3d73fff023f4541c9961de2f85cd3669
oleh <115097045+olehone@users.noreply.github.com>
2025-12-04 19:47:09 +0200

    Add simple DTO

--- Tracker.API/Program.cs
+++ Tracker.API/Program.cs
@@ -25,4 +25,4 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 
-app.Run();
+await app.RunAsync();
---
+++ Tracker.Domain/Mapping/UserMapping.cs
@@ -0,0 +1,19 @@
+﻿using Tracker.Domain.DTOs;
+using Tracker.Domain.Entities;
+namespace Tracker.Domain.Mapping;
+
+public static class UserMapping
+{
+    public static UserDto ToDto(this User user)
+    {
+        return new UserDto()
+        {
+            Id = user.Id,
+            Email = user.Email,
+            Username = user.Username,
+            FirstName = user.FirstName,
+            LastName = user.LastName,
+
+        };
+    }
+}
