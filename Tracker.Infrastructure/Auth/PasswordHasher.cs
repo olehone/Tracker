5f5ed11221bf6faa86cbdaa70bf5f5a073716e43
oleh <115097045+olehone@users.noreply.github.com>
2025-12-09 12:22:35 +0200

    fix(infrastructure): fix difference in methods of convertion from byte array to string

--- Tracker.Infrastructure/Auth/PasswordHasher.cs
+++ Tracker.Infrastructure/Auth/PasswordHasher.cs
@@ -23,8 +23,8 @@ internal class PasswordHasher : IPasswordHasher
     public bool Verify(string password, string passwordHash)
     {
         string[] parts = passwordHash.Split("-");
-        byte[] hash = Convert.FromBase64String(parts[0]);
-        byte[] salt = Convert.FromBase64String(parts[1]);
+        byte[] hash = Convert.FromHexString(parts[0]);
+        byte[] salt = Convert.FromHexString(parts[1]);
         byte[] inputHash = Rfc2898DeriveBytes.Pbkdf2(password, salt, Iterations, Algorithm, HashSize);
 
         return CryptographicOperations.FixedTimeEquals(hash, inputHash);
