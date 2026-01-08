c1f8b286f20a060eb1e3533f7cdb4f48689c2c06
oleh <115097045+olehone@users.noreply.github.com>
2026-01-08 21:17:01 +0200

    fix(domain): make Role property enum type for correct deserialization

--- frontend/Tracker.Domain/Dtos/UserDto.cs
+++ frontend/Tracker.Domain/Dtos/UserDto.cs
@@ -10,4 +10,6 @@ public class UserDto
     public required GlobalRole Role { get; set; }
     public required string FirstName { get; set; }
     public string? LastName { get; set; }
+
+    public string? AvatarUrl { get; set; }
 }
