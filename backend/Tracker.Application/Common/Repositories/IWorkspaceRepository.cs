30b3204ab806ceee8bbe862a31378f7683c04afc
oleh <115097045+olehone@users.noreply.github.com>
2026-01-09 17:29:13 +0200

    feat(persistence): add methods to take user workspace, mutual and not

--- backend/Tracker.Application/Common/Repositories/IWorkspaceRepository.cs
+++ backend/Tracker.Application/Common/Repositories/IWorkspaceRepository.cs
@@ -5,6 +5,9 @@ namespace Tracker.Application.Common.Repositories;
 public interface IWorkspaceRepository : IRepository<Workspace, Guid>
 {
     Task<IReadOnlyList<Workspace>> GetByUserAsync(Guid userId);
-    Task<IReadOnlyList<Workspace>> SearchByTitleAndUserAsync(
-        Guid userId, string title, int skip, int take);
+    Task<int> CountAsync(Guid? userId = null, string? title = null);
+    Task<List<Workspace>> GetAsync(
+        int skip, int take, Guid? userId = null, string? title = null);
+    Task<List<Workspace>> GetMutualAsync(
+        Guid targetUserId, Guid searchingUserId, int skip, int take, string? title = null);
 }
