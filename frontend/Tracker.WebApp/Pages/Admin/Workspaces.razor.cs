4e6a2befec5713b2c8313e31a57faec91b8852c7
oleh <115097045+olehone@users.noreply.github.com>
2026-01-09 23:44:58 +0200

    feat(ui): add admin workspace view

--- frontend/Tracker.WebApp/Pages/Admin/Workspaces.razor.cs
+++ frontend/Tracker.WebApp/Pages/Admin/Workspaces.razor.cs
@@ -1,11 +1,33 @@
 using Microsoft.AspNetCore.Components;
+using Tracker.API.Requests;
+using Tracker.Domain.Dtos;
 using Tracker.Services.Abstraction;
+using Tracker.WebApp.Components.Shared;
+using Tracker.WebApp.Shared;
 
 namespace Tracker.WebApp.Pages.Admin;
 
 public partial class Workspaces
 {
-    
     [Inject] IWorkspaceService WorkspaceService { get; set; } = null!;
+    [Inject] IErrorNotifier ErrorNotifier { get; set; } = null!;
+    [Inject] NavigationManager Nav { get; set; } = null!;
 
+    private ServerLoadingTable<WorkspaceSummaryDto>? _table;
+
+    private async Task<Paginated<WorkspaceSummaryDto>> LoadWorkspaces(
+        PaginatedSearchRequest request)
+    {
+        var result = await WorkspaceService.GetWorkspacesAsync(request);
+        if (ErrorNotifier.NotifyIfError(result))
+        {
+            return Paginated<WorkspaceSummaryDto>.Empty();
+        }
+        return result.Value;
+    }
+
+    private void OnSearchChanged(string search)
+    {
+        _table?.SetSearch(search);
+    }
 }
