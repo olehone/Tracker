ebfabf90fed1021d68a3ac07b9345112c24e7c96
oleh <115097045+olehone@users.noreply.github.com>
2026-01-09 22:13:43 +0200

    feat: add handling of api calls with id request and response

--- frontend/Tracker.Services.Abstraction/Results/IApiErrorHandler.cs
+++ frontend/Tracker.Services.Abstraction/Results/IApiErrorHandler.cs
@@ -6,6 +6,7 @@ namespace Tracker.Services.Abstraction.Results;
 public interface IApiErrorHandler
 {
     Task<Result> ExecuteAsync(Func<Task<ApiResponse<object>>> apiCall);
+
     Task<Result<TResponse>> ExecuteAsync<TResponse>(Func<Task<ApiResponse<TResponse>>> apiCall);
 
     Task<Result> ExecuteAsync<TRequest>(TRequest request,
@@ -13,7 +14,10 @@ public interface IApiErrorHandler
 
     Task<Result<TResponse>> ExecuteAsync<TRequest, TResponse>(TRequest request, Func<TRequest,
         Task<ApiResponse<TResponse>>> apiCall);
+
     Task<Result> ExecuteAsync<TId, TRequest>(TId idRequest, TRequest request, 
         Func<TId, TRequest, Task<ApiResponse<object>>> apiCall);
 
+    Task<Result<TResponse>> ExecuteAsync<TId, TRequest, TResponse>(TId idRequest, TRequest request, 
+        Func<TId, TRequest, Task<ApiResponse<TResponse>>> apiCall);
 }
