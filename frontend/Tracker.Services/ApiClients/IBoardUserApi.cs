315708aa6290d2e2f62598edd9d8af3c19adf1d9
oleh <115097045+olehone@users.noreply.github.com>
2026-01-14 23:31:37 +0200

    style: add empty lines

--- frontend/Tracker.Services/ApiClients/IBoardUserApi.cs
+++ frontend/Tracker.Services/ApiClients/IBoardUserApi.cs
@@ -8,12 +8,15 @@ public interface IBoardUserApi
 {
     [Get("/api/boards/{boardId}/users")]
     Task<ApiResponse<List<BoardUserDto>>> GetUsersByBoardAsync(Guid boardId);
+
     [Post("/api/boards/{boardId}/users/{userId}")]
     Task<ApiResponse<BoardUserDto>> AddUserToBoardAsync(Guid boardId, Guid userId,
         BoardUserRoleRequest request);
+
     [Put("/api/boards/{boardId}/users/{userId}")]
     Task<ApiResponse<object>> ChangeUserRoleAsync(Guid boardId, Guid userId,
         BoardUserRoleRequest request);
+
     [Delete("/api/boards/{boardId}/users/{userId}")]
     Task<ApiResponse<object>> RemoveUserFromBoardAsync(Guid boardId, Guid userId);
 }
