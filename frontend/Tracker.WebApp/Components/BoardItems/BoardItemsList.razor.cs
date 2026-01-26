77187f56fbf7d45466d6f1eb3cab8c526b8a32b8
oleh <115097045+olehone@users.noreply.github.com>
2026-01-27 00:43:16 +0200

    fix: removed drop zone for items without due date

--- frontend/Tracker.WebApp/Components/BoardItems/BoardItemsList.razor.cs
+++ frontend/Tracker.WebApp/Components/BoardItems/BoardItemsList.razor.cs
@@ -1,27 +1,12 @@
 using Microsoft.AspNetCore.Components;
 using Tracker.Domain.Dtos;
-using Tracker.WebApp.States;
 
 namespace Tracker.WebApp.Components.BoardItems;
 
 public partial class BoardItemsList
 {
-    [Parameter]
-    public bool CanCreate { get; set; } = true;
-
-    private BoardListDto? SelectedList { get; set; }
-
-    protected override void OnParametersSet()
-    {
-        base.OnParametersSet();
-        SelectedList = Lists[0];
-    }
-    private async Task CreateNewItem(string title)
-    {
-        if (SelectedList is null)
-        {
-            return;
-        }
-        await BoardState.ItemsState.CreateAsync(SelectedList.Id, title);
-    }
+    [Parameter,EditorRequired]
+    public string Title { get; set; }
+    [Parameter,EditorRequired]
+    public List<BoardItemDto> Items { get; set; }
 }
