b4971b0ad265cbc084d936c4c2ed1c7ee195b1be
oleh <115097045+olehone@users.noreply.github.com>
2026-01-27 15:17:02 +0200

    refactor(migration): rename userworkspaces to workspaceusers

---
+++ backend/Tracker.Database/Scripts/021-Rename-UserWorkspaces-To-WorkspaceUsers.sql
@@ -0,0 +1,28 @@
+﻿ALTER TABLE [dbo].[UserWorkspaces]
+DROP CONSTRAINT UQ_UserWorkspacess;
+
+ALTER TABLE [dbo].[UserWorkspaces]
+ADD CONSTRAINT UQ_WorkspaceUsers UNIQUE ([UserId], [WorkspaceId]);
+
+
+ALTER TABLE [dbo].[UserWorkspaces]
+DROP CONSTRAINT FK_UserWorkspaces_Users
+
+ALTER TABLE [dbo].[UserWorkspaces]
+ADD CONSTRAINT FK_WorkspaceUsers_Users
+        FOREIGN KEY (UserId)
+        REFERENCES [dbo].[Users](Id)
+        ON DELETE CASCADE;
+
+
+ALTER TABLE [dbo].[UserWorkspaces]
+DROP CONSTRAINT FK_UserWorkspaces_Workspaces
+
+ALTER TABLE [dbo].[UserWorkspaces]
+ADD CONSTRAINT FK_WorkspaceUsers_Workspaces
+        FOREIGN KEY (WorkspaceId)
+        REFERENCES [dbo].[Workspaces](Id)
+        ON DELETE CASCADE;
+
+
+EXEC sp_rename 'dbo.UserWorkspaces', 'WorkspaceUsers';
