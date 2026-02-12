191e659eb57ae2ced8fe11ca19dd46798f24b350
olehone <115097045+olehone@users.noreply.github.com>
2026-02-12 06:49:27 +0200

    feat: add second section for future comments and make description plain text again

--- frontend/Tracker.WebApp/Components/Items/ItemFull.razor.cs
+++ frontend/Tracker.WebApp/Components/Items/ItemFull.razor.cs
@@ -13,7 +13,7 @@ public partial class ItemFull
     {
         CloseButton = false,
         NoHeader = true,
-        MaxWidth = MaxWidth.Small
+        MaxWidth = MaxWidth.Medium
     };
 
     [CascadingParameter]
