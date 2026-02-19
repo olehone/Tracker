7032f32849740e9bd1b2c26dc70745d26e13636a
olehone <115097045+olehone@users.noreply.github.com>
2026-02-19 06:36:06 +0200

    feat: add chat messaging

---
+++ backend/Tracker.API/Hubs/IClientVideoCallHub.cs
@@ -0,0 +1,5 @@
+﻿namespace Tracker.API.Hubs;
+
+public interface IClientVideoCallHub
+{
+}
---
+++ backend/Tracker.API/Hubs/VideoCallHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Tracker.API.Hubs;
+
+[Authorize]
+public class VideoCallHub() : Hub<IClientVideoCallHub>
+{
+    public async Task JoinCall(Guid callId)
+    {
+        await Groups.AddToGroupAsync(Context.ConnectionId, $"call:{callId}");
+    }
+
+    public async Task LeaveCall(Guid callId)
+    {
+        await Groups.RemoveFromGroupAsync(Context.ConnectionId, $"call:{callId}");
+    }
+
+}
